02078384f4b1a0a5e1f889b55c5ee519d057873d
Monika Urbanska <monika.urbanska1@gmail.com>
2019-11-22 12:05:57 +0000

    Learning conditionals and loops with book catalogue.

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,42 +1,69 @@
 // eslint-disable-next-line no-unused-vars
 const catalogue = [
-  "The Catcher in the Rye by J.D. Salinger (10)",
-  "Dracula by Bram Stoker (0)",
-  "Between the Assassinations by Aravind Adiga (9)",
-  "Wolf Hall by Hilary Mantel (33)",
-  "Bring Up The Bodies by Hilary Mantel (31)",
-  "A Place of Greater Safety by Hilary Mantel (11)",
-  "Giving Up the Ghost by Hilary Mantel (8)",
-  "The Assassination of Margaret Thatcher by Hilary Mantel (43)",
-  "The Yellow Wallpaper by Charlotte Perkins Gilman (12)",
-  "Conversations With Friends by Sally Rooney (1)",
-  "Normal People by Sally Rooney (2)",
-  "Everything I Never Told You by Celeste Ng (6)",
-  "2666 by Robert Bolaño (17)",
-  "By Night In Chile by Robert Bolaño (8)",
-  "A Tale of Two Cities by Charles Dickens (3)",
-  "Oliver Twist by Charles Dickens (7)",
-  "Great Expectations by Charles Dickens (1)",
-  "The Blind Assassin by Margaret Atwood (8)",
-  "Why Be Happy When You Could Be Normal? by Jeanette Winterson (19)",
-  "The Origin of Species by Charles Darwin (50)"
+  { title: "The Catcher in the Rye", author: "J.D. Salinger", quantity: 10},
+  { title: "Dracula", author: "Bram Stoker", quantity: 0},
+  { title: "Between the Assassinations", auhor: "Aravind Adiga", quantity: 9},
+  { title: "Wolf Hall", author: "Hilary Mantel", quantity: 33},
+  { title: "Bring Up The Bodies", author: "Hilary Mantel", quantity: 31},
+  { title: "A Place of Greater Safety", author: "Hilary Mantel", quantity: 11},
+  { title: "Giving Up the Ghost", author: "Hilary Mantel", quantity: 8},
+  { title: "The Assassination of Margaret Thatcher", author: "Hilary Mantel", quantity: 43},
+  { title: "The Yellow Wallpaper", author: "Charlotte Perkins Gilman", quantity: 12},
+  { title: "Conversations With Friends", author: "Sally Rooney", quantity: 1},
+  { title: "Normal People", author: "Sally Rooney", quantity: 2},
+  { title: "Everything I Never Told You", author: "Celeste Ng", quantity: 6},
+  { title: "2666", author: "Robert Bolaño", quantity: 8},
+  { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8},
+  { title: "A Tale of Two Cities", author: "Charles Dickens", quantity: 7},
+  { title: "Oliver Twist", author: "Charles Dickens", quantity: 7},
+  { title: "Great Expectations", author: "Charles Dickens", quantity: 1},
+  { title: "The Blind Assassin", author: "Margaret Atwood", quantity: 8},
+  { title: "Why Be Happy When You Could Be Normal?", author: "Jeanette Winterson", quantity: 19},
+  { title: "The Origin of Species", author: "Charles Darwin", quantity: 50}
 ];
 
 function countBooksByAuthor(author) {
   // Your code here
-}
+    let bookByAuthor = 0;
+    for (let i = 0; i < catalogue.length; i++) {
+      let book = catalogue[i];
+      if (book.author === author) {
+        bookByAuthor++;
+      }
+    } return bookByAuthor;
+  };
 
 function checkBookByTitle(title) {
   // Your code here
-}
+  for (let i = 0; i < catalogue.length; i++) {
+    let book = catalogue[i];
+    if (book.title === title) {
+      return true;
+    }
+  } return false;
+};
 
 function countBooksByFirstLetter(letter) {
   // Your code here
-}
+  let matchingBook = 0;
+  for (let i = 0; i < catalogue.length; i++) {
+    let book = catalogue[i];
+    if (book.title[0] === letter || book.title[0] === letter.toUpperCase()) {
+      matchingBook++;
+    }
+  } return matchingBook;
+};
 
 function getQuantity(title) {
   // Your code here
-}
+  for (let i = 0; i < catalogue.length; i++) {
+    let book = catalogue[i];
+    if (book.title === title) {
+      return book.quantity;
+    }
+  }  
+  return "Sorry, we don't stock that book."
+};
 
 function getBooksByAuthor(author) {
   // Your code here
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,32 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists, false if it does not", () => {
+      expect(catalogueService.checkBookByTitle("The Assassination of Margaret Thatcher")).toBe(true);
+      expect(catalogueService.checkBookByTitle("Pride and Prejudice")).toBe(false);
+    });
+  });
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns total number of books that begin with given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("G")).toBe(2);
+      expect(catalogueService.countBooksByFirstLetter("T")).toBe(5);
+      expect(catalogueService.countBooksByFirstLetter("Z")).toBe(0);
+    });
+
+    test("still returns total number of books if given lowercase input", () => {
+      expect(catalogueService.countBooksByFirstLetter("w")).toBe(2);
+      expect(catalogueService.countBooksByFirstLetter("d")).toBe(1);
+    });
+  });
+
+  describe("catalogueService.getQuantity", () => {
+    test("return quantity of books in stock when given a title", () => {
+      expect(catalogueService.getQuantity("The Origin of Species")).toBe(50);
+      expect(catalogueService.getQuantity("Oliver Twist")).toBe(7);
+      expect(catalogueService.getQuantity("The Hobbit")).toBe("Sorry, we don't stock that book.");
+    });
+  });
 });
